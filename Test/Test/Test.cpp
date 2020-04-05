78dcf69d68a0141e3d27f2ef6ffeccbc0eceff9f
Ellyn-jia <15846706868@163.com>
2020-04-05 23:15:12 +0800

    4.5

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -37,20 +37,68 @@ using namespace std;
 
 //引用做返回值
 //若函数要使用引用返回，那么这个函数的返回值要出了定义域还在，否则不安全
-int& Count()
+//int& Count()
+//{
+//	static int a = 0;
+//	a++;
+//	return a;
+//}
+//int& Add(int a, int b)
+//{
+//	static int c = a + b; //如果没有static不安全
+//	return c;
+//}
+//int main()
+//{
+//
+//	int& ra = Count();
+//	return 0;
+//}
+
+
+
+//auto 
+
+//int main()
+//{
+//
+//	int a = 0;
+//	auto b = a;//b类型根据a推导
+//	auto& d = a;
+//
+//
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(d).name() << endl;
+//
+//	//范围for
+//	int arr[] = { 1, 2 };
+//	for (auto& e: arr)
+//	{
+//		e += 2;
+//	}
+//
+//	int* p = nullptr;//C++ NULL = 0
+//
+//	return 0;
+//
+//}
+
+//类
+
+class Person //默认私有  struct默认公有
 {
-	static int a = 0;
-	a++;
-	return a;
-}
-int& Add(int a, int b)
-{
-	static int c = a + b; //如果没有static不安全
-	return c;
-}
+public:
+	void Print() // 函数定义在类里面，默认是内联函数
+	{
+		cout << " " << endl;
+	}
+
+private:   //会被定义为私有或者隐私 不能被访问
+	char _name[10];
+	int _age;
+};
+
 int main()
 {
 
-	int& ra = Count();
-	return 0;
 }
