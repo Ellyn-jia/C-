99cd19ab24307ca008c0493b29beded9edce0592
Ellyn-jia <15846706868@163.com>
2020-04-22 22:21:52 +0800

    4.22

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -277,43 +277,146 @@ using namespace std;
 
 
 
-class Date
-{
-public:
+//class Date
+//{
+//public:
+//
+//	Date(int year, int month)
+//	{
+//		_year = year;
+//		_month = month;
+//	}
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//	}
+//	bool operator == (const Date& d)  //  bool operator == (Date* this, const Date& d)
+//	{
+//		return _year == d._year
+//			&& _month == d._month;
+//	}
+//	bool operator>(const Date& d)
+//	{
+//		if (_year > d._year)
+//			return true;
+//		else if (_year == d._year && _month > d._month)
+//			return true;
+//		return false;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//};
+//int main()
+//{
+//	Date d1(2020, 4);
+//	Date d2(2020, 5);
+//
+//	d1 == d2;
+//	d1 > d2;  //d1.operator>(d2)
+//	return 0;
+//}
 
-	Date(int year, int month)
+
+
+class Date{
+public:
+	int GetMonthDay(int year, int month)
 	{
-		_year = year;
-		_month = month;
+		static int MonthDay[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))
+			return 29;
+		return MonthDay[month];
 	}
-	Date(const Date& d)
+	Date(int year, int month, int day)
 	{
-		_year = d._year;
-		_month = d._month;
+		if (year >= 0 && month >= 1 && month <= 12 && day >= 1 && day <= GetMonthDay(year, month))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+			cout << "非法日期" << endl;
 	}
-	bool operator == (const Date& d)  //  bool operator == (Date* this, const Date& d)
+	void Print()
 	{
-		return _year == d._year
-			&& _month == d._month;
+		cout << _year << "-" << _month << "-" << _day << endl;
 	}
-	bool operator>(const Date& d)
+	bool operator < (const Date& d)
 	{
-		if (_year > d._year)
+		if (_year < d._year)
 			return true;
-		else if (_year == d._year && _month > d._month)
+		else if (_year == d._year && _month < d._month)
+			return true;
+		else if (_year == d._year && _month == d._month && _day < d._day)
 			return true;
 		return false;
+	}
+	bool operator ==(const Date& d)
+	{
+		return _year == d._year && _month == d._month && _day == d._day;
+	}
+	bool operator <= (const Date& d)
+	{
+		return *this < d || *this == d;
+	}
+	bool operator >(const Date& d)
+	{
+		return !(*this <= d);
+	}
+	bool operator >= (const Date& d)
+	{
+		return !(*this < d);
+	}
+	bool operator != (const Date& d)
+	{
+		return !(*this == d);
+	}
+	Date operator+(int day)
+	{
+		Date ret(*this);
+		ret._day += day;
+		while (ret._day > GetMonthDay(ret._year, ret._month))
+		{
+			ret._day -= GetMonthDay(ret._year, ret._month);
+			ret._month++;
+			if (ret._month == 13)
+			{
+				ret._year++;
+				ret._month = 1;
+			}
+		}
+		return ret;
+	}
+	Date operator+=(int day)
+	{
+		_day += day;
+		while (_day > GetMonthDay(_year, _month))
+		{
+			_day -= GetMonthDay(_year, _month);
+			_month++;
+			if (_month == 13)
+			{
+				_year++;
+				_month = 1;
+			}
+		}
+		return *this;
 	}
 private:
 	int _year;
 	int _month;
+	int _day;
 };
 int main()
 {
-	Date d1(2020, 4);
-	Date d2(2020, 5);
-
-	d1 == d2;
-	di > d2;  //d1.operator>(d2)
-	return 0;
+	Date d1(2021, 2, 29);
+	Date d2(2020, 2, 29);
+	d2.Print();
+	Date d3(2018, 3, 3);
+	d3.Print();
+	Date d4 = d2+10;
+	d4.Print();
 }
